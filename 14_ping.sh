5d0c274f853d7d4604b71cfb2957542609b1c1a8
ncs10322 <112328288+ncs10322@users.noreply.github.com>
2023-09-19 16:05:55 +0900

    Add files via upload

--- 14_ping.sh
+++ 14_ping.sh
@@ -2,12 +2,12 @@
 echo "=================================="
 echo "========= EC2 Ping Test =========="
 echo "=================================="
-cat EC2_list.txt | while read output
+cat /home/ec2-user/EC2_list.txt | while read IP_ADDRESS
 do
-    ping -c 1 -W 1 "$output" > /dev/null
+    ping -c 1 -W 1 "$IP_ADDRESS" > /dev/null
     if [ $? -eq 0 ]; then
-    echo "\"$output\" EC2 Instance is up"
+    echo "\"$IP_ADDRESS\" EC2 Instance is up"
     else
-    echo "\"$output\" EC2 Instance is down"
+    echo "\"$IP_ADDRESS\" EC2 Instance is down"
     fi
 done
