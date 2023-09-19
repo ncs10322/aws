969454414a9609a6900f385dfcfee9f17480ad8f
ncs10322 <112328288+ncs10322@users.noreply.github.com>
2023-09-19 14:03:58 +0900

    Add files via upload

---
+++ 14_ping.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date
+cat EC2_list.txt | while read output
+do
+    ping -c 1 -W 1 "$output" > /dev/null
+    if [ $? -eq 0 ]; then
+    echo "\"$output\" EC2 Instance is up"
+    else
+    echo "\"$output\" EC2 Instance is down"
+    fi
+done
